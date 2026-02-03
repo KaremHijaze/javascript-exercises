aa2527762eb5222ae93c726bc0ee1000465b7a74
Karem Hijaze <126591818+KaremHijaze@users.noreply.github.com>
2026-02-03 22:52:37 +0200

    Add ex 6 sol in foundations in javascript_exercises
    Added my solution to 06_reapeatString in
    javascript_exercises in foundations, where different
    Array and String methods were used to solve the given
    problem according to requirements and pass the tests.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
+    if (!str || str.length === 0) return "";
+    
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr = arr.concat(str.split(""));
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
