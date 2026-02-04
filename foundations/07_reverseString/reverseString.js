e5d3195059958e8a1c221ba684f6bbffe929fc13
Karem Hijaze <126591818+KaremHijaze@users.noreply.github.com>
2026-02-04 23:17:08 +0200

    Add ex 7 sol in foundations in javascript-exercises
    added my solution to 07_reverseString exercise in
    foundations in javascript-exercises, where a function
    to reverse a given string was implemented and tested
    for edge cases and potentail incorrect outputs.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    if (!str || str.length === 0) return "";
 
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
