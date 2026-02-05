761d1803226696c843abdfb36e7808192ca85705
Karem Hijaze <126591818+KaremHijaze@users.noreply.github.com>
2026-02-05 23:58:41 +0200

    Add ex 9 sol in foundations in javascript-exercises
    Added my solution to 09_sumAll exercise in foundations
    in javascript-exercises, where a function was implemented
    that takes two arguments and return the sum of all
    numbers between the two arguments if they numbers
    and "ERROR" otherwise.
    the function passed all the given tests and edge cases.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if( !Number.isInteger(start) ||
+        !Number.isInteger(end) ||
+        start < 0 || 
+        end < 0) {
+            return "ERROR";
+    }
 
+    [minNum, maxNum] = start < end ? [start, end] : [end, start];
+    let sum = 0;
+    for (let n = minNum; n <= maxNum; n++) {
+        sum += n;
+    }
+    return sum;
 };
 
 // Do not edit below this line
