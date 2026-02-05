0d536916bfbfe034a84a25e6422f59903860f83d
Karem Hijaze <126591818+KaremHijaze@users.noreply.github.com>
2026-02-05 23:26:15 +0200

    Add ex 8 sol in foundations in javascript-exercises
    Added my solution to 08_removeFromArray exercise in
    foundations in javascript-exercises, where a function
    to remove given elements from the array was implemeneted
    and tested for correctness including edge cases.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    if (!Array.isArray(arr) || arr.length === 0) return arr;
+
+    return arr.filter(item => !elements.includes(item));
 };
 
 // Do not edit below this line
